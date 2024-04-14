32da9c67fda6c9689b821b69be3198248b24e7f6
Michał <108900680+Mdrozak@users.noreply.github.com>
2024-04-14 22:19:52 +0200

    Add new faeture of changing grid size

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 
 // Get #container selector
 let divContainer = document.querySelector("#container")
+let inputButton = document.querySelector("#grid-button")
 
 function createDivGrid (grid) {
 
@@ -14,19 +15,41 @@ function createDivGrid (grid) {
 
 }
 
+
 // 2. Use flexbox for those divs
 // added in style css
 
 // 3. Add hover effect to change divs color when mouse passes over them
 // leaving pixelated trial through grid
 divContainer.addEventListener("mouseover", function (event) {
-    event.target.id === "container" ? "" : event.target.style.background = "red"
+    event.target.id === "container" ? null : event.target.style.background = "red"
 })
 
 // 4. Add button on top, that triggers popup for user input for square number per side for new grid
 // after entering, remove existing grid and generate new grid in the same total space as before
+inputButton.addEventListener("click", getInput)
 
-// 5. Limit for user input 100
+function getInput () {
+    let userInput = parseInt(prompt("Please input desired grid size between 1 and 100"))
+    // 5. Limit for user input 100
+    userInput > 100 || userInput <= 0 ? alert("Please input value between 1 and 100") : resetGrid(userInput)
+    
+}
+
+function removeGridChildren () {
+    let divChildren = divContainer.childNodes
+    
+    while (divChildren.length > 0) {
+        divContainer.removeChild(divContainer.firstChild)
+    }
+}
+
+function resetGrid (input) {
+    removeGridChildren()
+    createDivGrid(input)
+}
+
+
 
 // Extra - Add randomize color squares RGB value with each interaction
 // Extra - Add progressive darkening effect, where each interaction dakrens the square by 10%,
